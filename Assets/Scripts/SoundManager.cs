06a2af2a1dfde0f799c6a56901689deb0bfe1965
JimmyCitrus <59841873+JimmyCitrus@users.noreply.github.com>
2020-10-13 11:17:02 -0400

    Added background music. Created the SoundManager object and script

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance = null;
+    private AudioSource soundEffectAudio;
+
+    //SFX
+    public AudioClip gunFire;
+    public AudioClip upgradedGunFire;    public AudioClip hurt;
+    public AudioClip alienDeath;
+    public AudioClip marineDeath;
+    public AudioClip victory;
+    public AudioClip elevatorArrived;
+    public AudioClip powerUpPickup;
+    public AudioClip powerUpAppear;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Ensuring there is only one copy of the instance
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        //Checks for no audio in the source before setting the next audio
+        AudioSource[] sources = GetComponents<AudioSource>();
+        foreach (AudioSource source in sources)
+        {
+            if (source.clip == null)
+            {
+                soundEffectAudio = source;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
